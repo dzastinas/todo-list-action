b1ceb223677b6b9c0d52280414d95cfadf17bfdd
justinask <dzastinas@gmail.com>
2017-11-22 19:22:59 +0200

    todo app

---
+++ src/main/java/model/ToDoItem.java
@@ -0,0 +1,28 @@
+package model;
+
+public class ToDoItem implements Comparable<ToDoItem> {
+    private Long id;
+    private String name;
+    private boolean completed;
+
+    @Override
+    public int compareTo(ToDoItem toDoItem) {
+        return 0;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
